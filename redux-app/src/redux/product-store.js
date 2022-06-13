b02ee7b4df816b4ad16ddf67c405ae19199a5ceb
Ronan <ronan.lavorato@gmail.com>
2022-06-13 02:12:26 -0300

    configure store

--- redux-app/src/redux/product-store.js
+++ redux-app/src/redux/product-store.js
@@ -1,7 +1,7 @@
-import {createStore} from 'redux';
+import { configureStore } from 'redux';
 import reducers from './reducers/index';
 
-const prodStore = createStore(reducers, {},window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());; // 2nd parametro  is the state
+const prodStore = configureStore(reducers, {},window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());; // 2nd parametro  is the state
 
 export default prodStore;
 
